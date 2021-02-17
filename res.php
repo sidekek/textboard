1bd9580d64a65292baefeb3c034b9fbb45f0eb9c
BuildTools <buffer@sidekek.ml>
2021-02-17 12:22:28 -0500

    better date formatting

--- res.php
+++ res.php
@@ -2,6 +2,6 @@
 $thread = json_decode(file_get_contents("data/posts.json"));
 foreach ($thread->posts as $post)
 {
-    echo("<div class='post'><span class='post-top'><b>".htmlspecialchars($post->name)."</b> no. ".htmlspecialchars($post->id)." <i>".date("c", $post->time)."</i></span><br><p>".htmlspecialchars($post->body)."</p></div>");
+    echo("<div class='post'><span class='post-top'><b>".htmlspecialchars($post->name)."</b> no. ".htmlspecialchars($post->id)." <i>".date("Y-m-d H:i:s", $post->time)."</i></span><br><p>".htmlspecialchars($post->body)."</p></div>");
 }
 ?>
