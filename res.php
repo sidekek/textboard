48598f50dbb57f815a5709c006526fa9e8fd7ef0
BuildTools <buffer@sidekek.ml>
2021-02-18 11:26:25 -0500

    pre tag

--- res.php
+++ res.php
@@ -2,6 +2,6 @@
 $thread = json_decode(file_get_contents("data/posts.json"));
 foreach ($thread->posts as $post)
 {
-    echo("<div class='post'><span class='post-top'><b>".htmlspecialchars((isset($post->name) ? $post->name : "anon"))."</b> no. ".htmlspecialchars((isset($post->id) ? $post->id : 1337))." <i>".date("Y-m-d H:i:s", (isset($post->time) ? $post->time : 0))."</i></span><br><p>".htmlspecialchars((isset($post->body) ? $post->body : "PHALLUSES EVERYWHERE"))."</p></div>");
+    echo("<div class='post'><span class='post-top'><b>".htmlspecialchars((isset($post->name) ? $post->name : "anon"))."</b> no. ".htmlspecialchars((isset($post->id) ? $post->id : 1337))." <i>".date("Y-m-d H:i:s", (isset($post->time) ? $post->time : 0))."</i></span><br><pre>".htmlspecialchars((isset($post->body) ? $post->body : "PHALLUSES EVERYWHERE"))."</pre></div>");
 }
 ?>
